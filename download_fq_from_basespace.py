b18a584bcf66c5807492a19e358ad4aee0374c3b
Philipp A <flying-sheep@web.de>
2018-12-06 13:30:11 +0000

    Add proper CLI, allow listing projects

--- download_fq_from_basespace.py
+++ download_fq_from_basespace.py
@@ -1,38 +1,63 @@
+#!/usr/bin/env python2.7
+
+from __future__ import print_function
+
 import sys, os, glob, logging
+from argparse import ArgumentParser
+
 from BaseSpacePy.api.BaseSpaceAPI import BaseSpaceAPI
 from BaseSpacePy.model.QueryParameters import QueryParameters as qp
-listOptions = qp({'Limit': 1024})
 
-logging.basicConfig(level=logging.INFO,
-                    format='[%(asctime)s] %(message)s',
-                    datefmt='%d/%m/%Y %H:%M:%S')
-myAPI = BaseSpaceAPI()
 
-p = sys.argv[1]
-user = myAPI.getUserById('current')
-logging.info("User Name: %s" % str(user))
-projects = myAPI.getProjectByUser(listOptions)
+list_options = qp({'Limit': 1024})
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] %(message)s',
+    datefmt='%d/%m/%Y %H:%M:%S',
+)
+bs = BaseSpaceAPI()
+
+
+user = bs.getUserById('current')
+logging.info("User Name: %s", user)
+projects = bs.getProjectByUser(list_options)
 project_list = [project.Name for project in projects]
 
+cli = ArgumentParser()
+cli.add_argument('project', nargs='?', help='Which project to download files from. When not specified, list projects instead.')
+cli.add_argument('--dry-run', '-n', action='store_true', help='Only show which files would be downloaded without downloading them.')
+cli.add_argument('--dir', '-d', default='.', help='Directory to download samples to.')
+args = cli.parse_args()
+
+if not args.project:
+    print(*project_list, sep='\n')
+    sys.exit(0)
+
+p = args.project
+
 try:
-    idx = project_list.index(sys.argv[1])
+    idx = project_list.index(p)
     project = projects[idx]
 except ValueError:
-    message = '"%s" is not in your projects. Available projects are:\n%s' % \
-              (sys.argv[1],
-               '\n'.join(project_list))
-    logging.error(message)
+    logging.error(
+        '%r is not in your projects. Available projects are:\n%s',
+        p, '\n'.join(project_list),
+    )
     sys.exit(1)
 
-downloaded = glob.glob('*fastq.gz') # get already downloaded fastq
+# get already downloaded fastq
+downloaded = {f.split('/')[-1] for f in glob.glob(args.dir + '/*fastq.gz')}
 
-logging.info("Retrieving samples from project %s" % sys.argv[1])
-samples=project.getSamples(myAPI,listOptions)
-logging.info("Samples for this project: %s" % str(samples))
+logging.info("Retrieving samples from project %s", p)
+samples = project.getSamples(bs, list_options)
+logging.info("Samples for this project: %s", samples)
 
 for sample in samples:
-    logging.info("Retrieving files in sample %s" % str(sample))
-    for f in sample.getFiles(myAPI):
+    logging.info("Retrieving files in sample %s", sample)
+    for f in sample.getFiles(bs):
         if f.Name not in downloaded:
-            logging.info("Downloading file %s" % f.Name)
-            f.downloadFile(myAPI, '.')
+            logging.info("Downloading file %s", f.Name)
+            if args.dry_run:
+                continue
+            f.downloadFile(bs, args.dir)
